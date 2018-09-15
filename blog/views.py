b6f6dcd1d54f0eb0cf85945053d07af9eac14174
ohsehee <o.1_@hanmail.net>
2018-09-15 16:58:01 +0900

    z

--- blog/views.py
+++ blog/views.py
@@ -24,9 +24,10 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_new(request):
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -34,5 +35,5 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
